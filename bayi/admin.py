91420eee03afbb72c1eda62b9d60b6b7697643c9
erdememinaga <erdememinaga1893@gmail.com>
2020-12-20 03:18:31 +0300

    hatalar

--- bayi/admin.py
+++ bayi/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
-# Register your models here.
+    # from django.contrib import admin
+    # from .models import Urunler, Bakim, Hammadde, Bayiler ,Recete, Bayiler_Siparis , Bayiler_Odeme , Ebat
+    #
+    # # Register your models here.
+    # admin.site.register(Urunler)
+    # admin.site.register(Bakim)
+    # admin.site.register(Hammadde)
+    # admin.site.register(Recete)
+    # admin.site.register(Bayiler)
+    # admin.site.register(Bayiler_Siparis)
+    # admin.site.register(Bayiler_Odeme)
+    # admin.site.register(Ebat)

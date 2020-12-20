c2ea7e1cd23108da2d3c4e27f7314cb75c689a1b
tk0798 <34775797+tk0798@users.noreply.github.com>
2020-12-20 17:41:01 +0300

    bayi admin ve model düzenleme

--- bayi/admin.py
+++ bayi/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import Urunler
-admin.site.register(Urunler)
 
-# from.models import Modeller
-# admin.site.register(Modeller)
-from.models import Bayiler,Bakim,Hammadde,Recete,Bayiler_Siparis,Bayiler_Odeme,Ebat
+class UrunlerAdmin(admin.ModelAdmin):
+    list_display = ('urun_ad','ebat','fiyat') # gösterilecek olan parametreler
+    list_filter = ('status',) #durumuna göre sıralama
+    ordering = ('urun_ad','-update_at') #sıralama
+    search_fields = ('urun_ad',) #arama
+    prepopulated_fields = {'slug':('urun_ad',)} #başlıkla bağlantılı slug
+    list_per_page = 20 # sayfa başına kaç madde geleceği
+
+
+from.models import Urunler,Bayiler,Bakim,Hammadde,Recete,Bayiler_Siparis,Bayiler_Odeme,Ebat
+admin.site.register(Urunler,UrunlerAdmin)
 admin.site.register(Bayiler)
 admin.site.register(Bakim)
 admin.site.register(Hammadde)
--- bayi/models.py
+++ bayi/models.py
@@ -20,6 +20,10 @@ class Modeller(models.Model):
 class Ebat(models.Model):
     ebat_ad = models.CharField(max_length=30)
 
+    class Meta:
+        verbose_name = 'Ebat'
+        verbose_name_plural = 'Ebatlar'
+
     def __str__(self):
         return self.ebat_ad
 
@@ -41,6 +45,10 @@ class Urunler(models.Model):
     update_at = models.DateTimeField(auto_now=True)
    # modeller = models.ForeignKey(Modeller, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'Ürün'
+        verbose_name_plural = 'Ürünler'
+
 
     def __str__(self):
         return self.urun_ad
@@ -55,6 +63,9 @@ class Bakim(models.Model):
     islem_turu = models.CharField(max_length=30)
     maliyet = models.IntegerField()
     sure = models.TimeField()
+    class Meta:
+        verbose_name = 'Bakım'
+        verbose_name_plural = 'Bakımlar'
 
 
     def __str__(self):
@@ -76,6 +87,10 @@ class Hammadde(models.Model):
     stok_adet = models.IntegerField()
     fiyat = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'Hammadde'
+        verbose_name_plural = 'Hammaddeler'
+
     def __str__(self):
         return self.hammadde_ad
 
@@ -88,7 +103,11 @@ class Recete(models.Model):
 
     #hammadde = models.ForeignKey(Hammadde, on_delete=models.CASCADE)
     hammadde = models.ForeignKey(Hammadde, on_delete=models.CASCADE)
-    kullanim = models.CharField(max_length=50)
+    kullanim = models.TextField()
+
+    class Meta:
+        verbose_name = 'Reçete'
+        verbose_name_plural = 'Reçeteler'
 
     # def __str__(self):
     #     return self.islem_tur
@@ -100,21 +119,36 @@ class Bayiler(models.Model):
     bayi_mail = models.CharField(max_length=30)
     sifre = models.CharField(max_length=30)
 
+    class Meta:
+        verbose_name = 'Bayi'
+        verbose_name_plural = 'Bayiler'
+
     def __str__(self):
         return self.bayi_ad
 
 
 class Bayiler_Siparis(models.Model):
-    #urunler = models.ForeignKey(Urunler,on_delete=models.CASCADE)
+
     bayiler = models.ForeignKey(Bayiler,on_delete=models.CASCADE)
+    urunler = models.ForeignKey(Urunler, on_delete=models.CASCADE)
     adet = models.IntegerField()
     tarih = models.DateTimeField()
 
+    class Meta:
+        verbose_name = 'Bayi Sipariş'
+        verbose_name_plural = 'Bayi Siparişleri'
+    #
+    # def __str__(self):
+    #     return self.
 
 
 class Bayiler_Odeme(models.Model):
     bayiler_siparis = models.ForeignKey(Bayiler_Siparis,on_delete=models.CASCADE)
     odeme_turu = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Bayi Ödeme'
+        verbose_name_plural = 'Bayi Ödemeleri'
 
     def __str__(self):
         return self.odeme_turu

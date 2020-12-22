eb633a86e2f38c1fd94bc1f6c50599ee7905601a
erdememinaga <erdememinaga1893@gmail.com>
2020-12-23 01:26:20 +0300

    model duzenlendi

--- lift/admin.py
+++ lift/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from lift.models import Urun,Siparis,Hammadde,Odeme,Recete,Bakim
+
+admin.site.register(Urun)
+admin.site.register(Siparis)
+admin.site.register(Hammadde)
+admin.site.register(Odeme)
+admin.site.register(Recete)
+admin.site.register(Bakim)
--- lift/models.py
+++ lift/models.py
@@ -1,3 +1,87 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class Urun(models.Model):
+    urun_adi = models.CharField(max_length=100,blank=True)
+    urun_aciklama = models.CharField(max_length=100,blank=True)
+    # urun_resim = models.ImageField()
+    model = models.CharField(max_length=100,blank=True)
+    ebat = models.CharField(max_length=100,blank=True)
+    fiyat = models.IntegerField()
+    fiyat_oran =models.IntegerField(blank=True)
+    class Meta:
+        verbose_name = 'Ürün'
+        verbose_name_plural = 'Ürünler'
+
+    def __str__(self):
+        return self.urun_adi
+class Siparis(models.Model):
+    bayi = models.ForeignKey('auth.User', verbose_name='bayi', on_delete=models.CASCADE, related_name='bayi',limit_choices_to={'groups__name': "BayiGrubu"})
+    urun = models.ForeignKey(Urun, on_delete=models.CASCADE)
+    adet = models.IntegerField()
+    tarih = models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Bayi Sipariş'
+        verbose_name_plural = 'Bayi Siparişleri'
+
+        def __str__(self):
+            return self.bayi
+
+class Odeme(models.Model):
+    bayiler_siparis = models.ForeignKey(Siparis,on_delete=models.CASCADE)
+    odeme_turu = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Bayi Ödeme'
+        verbose_name_plural = 'Bayi Ödemeleri'
+
+    def __str__(self):
+        return self.odeme_turu
+class Hammadde(models.Model):
+
+    hammadde_ad = models.CharField(max_length=30)
+    temin_sure = models.DateField()
+    stok_adet = models.IntegerField()
+    fiyat = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Hammadde'
+        verbose_name_plural = 'Hammaddeler'
+
+    def __str__(self):
+        return self.hammadde_ad
+
+class Recete(models.Model):
+
+    urunler = models.ForeignKey(Urun, on_delete=models.CASCADE)
+
+    #hammadde = models.ForeignKey(Hammadde, on_delete=models.CASCADE)
+    hammadde = models.ForeignKey(Hammadde, on_delete=models.CASCADE)
+    adet = models.IntegerField()
+    char = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Reçete'
+        verbose_name_plural = 'Reçeteler'
+
+        def __str__(self):
+            return self.char
+
+class Bakim(models.Model):
+
+    urunler = models.ForeignKey(Urun,on_delete=models.CASCADE)
+    islem_turu = models.CharField(max_length=30)
+    maliyet = models.IntegerField()
+    sure = models.DateField()
+    class Meta:
+        verbose_name = 'Bakım'
+        verbose_name_plural = 'Bakımlar'
+
+
+    def __str__(self):
+        return self.islem_turu
